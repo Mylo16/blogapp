43cdbb9695b80f5b8f787e4f7001194239497ba8
eric <ericopokuantwi384@gmail.com>
2023-07-27 22:24:01 +0000

    finish view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def show; end
+  def show
+    @user = User.find_by(id: params[:user_id])
+    @post = Post.find_by(id: params[:id])
+  end
 
-  def index; end
+  def index
+    @user = User.find_by(id: params[:user_id])
+  end
 end
