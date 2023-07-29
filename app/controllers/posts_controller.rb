dfe33cb72d64a1d8422a1a2d3f315719648aa22f
eric <ericopokuantwi384@gmail.com>
2023-07-29 07:50:19 +0000

    finish forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,27 @@ class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully'
+    else
+      flash.now[:alert] = 'Error: Post could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
