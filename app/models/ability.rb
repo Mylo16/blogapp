76edeec91fa05be3a8cfc239e7dbc6d9707eac36
Naan Ahmed Mohammed <124832435+naanmohammed@users.noreply.github.com>
2023-08-04 09:42:56 +0300

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
+class Ability
+  include CanCan::Ability
 
-
+  def initialize(user)
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, Comment, author_id: user.id
+      can :create, Post, author_id: user.id
+      can :create, Like
+      can :destroy, Post do |post|
+        post.author_id == user.id
+      end
+      can :destroy, Comment do |comment|
+        comment.author_id == user.id
+      end
+    end
+  end
+end
